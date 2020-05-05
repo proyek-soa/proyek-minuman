83b027bc8a2eb85848310e32baf143b62125df45
Leonardi <kimsun1259@gmail.com>
2020-05-05 12:41:22 +0700

    login register search sama history

---
+++ index.js
@@ -0,0 +1,162 @@
+const express = require("express");
+const mysql = require("mysql");
+const app = express();
+const jwt = require("jsonwebtoken");
+const request = require("request");
+const pool = mysql.createPool({
+    host:"localhost",
+    database:"proyek_soa",
+    user:"root",
+    password:""
+})
+function getminuman(title) {
+    return new Promise(function(resolve, reject) {
+        var options = {
+            'method': 'GET',
+            'url': `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${title}`
+          };
+        request(options, function (error, response) { 
+        if (error) reject(error);
+        resolve(response.body);
+        });
+    });
+}
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+app.post('/api/register',(req,res)=>{
+    var temp=req.body.username;
+
+    if(!req.body.username||!req.body.password){
+        res.status(400).send("Bad Request : 400");
+    }
+    else if(!req.body.username||!req.body.password){
+        res.status(400).send("Status :400, Pastikan Parameter terisi");
+    }
+    else{
+        var query="select * from user where username='"+req.body.username+"'";
+        pool.query(query,(err,rows,fields)=>{
+            if(err)console.log(err);
+            else{
+                if(rows.length==1){
+                    res.status(400).send("status: 400, Pendaftaran gagal,username sudah terdaftar");
+                }else{
+                    var querys= "insert into user values('','"+temp+"','"+req.body.password+"',0)"
+        
+                    pool.query(querys,(err,rows,fields)=>{
+                        if(err)console.log(err);
+                        else{
+                            res.status(200).send("status: 200, Pendaftaran Berhasil");
+                        }
+                        
+                    });
+                }
+            }})
+
+        
+    }    
+});
+app.post("/api/login",function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+    pool.getConnection(function(err,conn){
+        if(err) res.status(500).send(err);
+        else{
+            conn.query(`select * from user where username='${username}' and password ='${password}'`,function(error,result){
+                if(error ) res.status(500).send(error);
+                else{
+                    
+                    if(result.length <=0){
+                        return res.status(400).send("Invalid username or password");
+                    }
+                    console.log(result[0].id_user)
+                    const token = jwt.sign({    
+                        "id":result[0].id_user,
+                        "membership":result[0].status,
+                    }   ,"minuman");
+                    console.log(token.id)
+                    res.status(200).send(token);
+                }
+            })
+        }
+    });
+});
+app.get("/api/searchminuman/:namaminuman/:username",async function(req,res){
+    var ctr=1;
+    const token = req.header("x-auth-token");
+    let user = {};
+    var namaminuman=req.params.namaminuman;
+    if(!token){
+        ctr=0;
+        res.status(401).send("Token not found");
+    }
+    try{
+        user = jwt.verify(token,"minuman");
+    }catch(err){
+        ctr=0;
+        res.status(401).send("Token Invalid");
+    }
+    if((new Date().getTime()/1000)-user.iat>3*86400){
+        ctr=0;
+        return res.status(400).send("Token expired");
+    }
+    if (ctr==1) {
+ 
+       
+        var query= "insert into history_search values('"+req.params.username+"','"+req.params.namaminuman+"')"
+        
+        pool.query(query,(err,rows,fields)=>{
+            if(err)console.log(err);
+            else{
+               
+            }
+            
+        });
+        
+        var hasil = await getminuman(req.params.namaminuman);
+        const parsing=JSON.parse(hasil);
+        res.status(200).send(parsing)
+       
+
+}
+    
+});
+app.get("/api/viewsearchhistory/:username",async function(req,res){
+    var ctr=1;
+    const token = req.header("x-auth-token");
+    let user = {};
+    var namaminuman=req.params.namaminuman;
+    if(!token){
+        ctr=0;
+        res.status(401).send("Token not found");
+    }
+    try{
+        user = jwt.verify(token,"minuman");
+    }catch(err){
+        ctr=0;
+        res.status(401).send("Token Invalid");
+    }
+    if((new Date().getTime()/1000)-user.iat>3*86400){
+        ctr=0;
+        return res.status(400).send("Token expired");
+    }
+    if (ctr==1) {
+ 
+       
+        pool.getConnection(function(err,conn){
+            if(err) res.status(500).send(err);
+            else{
+                conn.query(`select search from history_search where username='${req.params.username}'`,function(error,result){
+                    if(error ) res.status(500).send(error);
+                    else{
+                        res.send(result)
+                    }
+                })
+        }})
+        
+
+}
+    
+});
+app.listen(3000);
+console.log("listening to hosts 3000");
---
+++ proyek_soa.sql
@@ -0,0 +1,91 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 05 Bulan Mei 2020 pada 07.38
+-- Versi server: 10.4.6-MariaDB
+-- Versi PHP: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `proyek_soa`
+--
+CREATE DATABASE IF NOT EXISTS `proyek_soa` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `proyek_soa`;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `history_search`
+--
+
+DROP TABLE IF EXISTS `history_search`;
+CREATE TABLE `history_search` (
+  `username` varchar(255) NOT NULL,
+  `search` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `history_search`
+--
+
+INSERT INTO `history_search` (`username`, `search`) VALUES
+('test', 'margaritas'),
+('test', 'margaritas');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id_user`, `username`, `password`, `status`) VALUES
+(1, 'test', '124', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
